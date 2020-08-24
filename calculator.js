6c9e8537db1491b778c9898b7b41e62ffadce39f
nferenczy <67202414+nferenczy@users.noreply.github.com>
2020-08-24 17:02:11 +0100

    created HTML, CSS and Js files
    Added basic Js calculators

---
+++ calculator.js
@@ -0,0 +1,19 @@
+prompt("What's your name?");
+prompt("What's their name?");
+
+let loveScore = Math.random() + 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+    alert("Your love score is" + loveScore + "%. You are a perfect match!")
+} if (loveScore > 30 && loveScore <= 70) {
+    alert("Your love score is" + loveScore + "%. You might be okay, but it'll be tough!");
+}
+else {
+    alert("Your love score is" + loveScore + "%");
+}
+
+
+
+
+
