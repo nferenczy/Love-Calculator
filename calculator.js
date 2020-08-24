a50fa3478107afaf4bb7a32952b150714f3b4c27
nferenczy <67202414+nferenczy@users.noreply.github.com>
2020-08-24 17:13:27 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ prompt("What's their name?");
 let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
-if (loveScore > 70) {
+if (loveScore > 80) {
     alert("Your love score is" + loveScore + "%. You are a perfect match!")
 } if (loveScore > 30 && loveScore <= 70) {
     alert("Your love score is" + loveScore + "%. You might be okay, but it'll be tough!");
